f8f9a5d8e30ca35e6dabd9e455e206a0555c2cf2
Stu Heiss <sheiss@highlandsolutions.com>
2019-06-16 17:16:44 -0500

    Add delay on Auto moves

--- src/Main.elm
+++ src/Main.elm
@@ -2,13 +2,16 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, h1, h2, text, div, table, thead, th, tr, td, ul, li, button)
+import Html exposing (Html, h1, h2, h3, text, div, table, thead, th, tr, td, ul, li, button)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (class, style)
 import String
 import Array
 import Random
 import Random.List exposing (shuffle)
+import Delay
+import Time
+import Task
 
 
 -- MAIN
@@ -39,6 +42,11 @@ type alias Model =
   , winner: Winner
   , automoves: List Int
   , rand: Int
+  , color : String
+  , colorCycling : Bool
+  , zone : Time.Zone
+  , time : Time.Posix
+  , string: String
   }
 
 
@@ -52,11 +60,16 @@ init _ =
     , winner = initWinner
     , automoves = List.range 0 8 -- ordered
     , rand = 0
+    , color = ""
+    , colorCycling = False
+    , zone = Time.utc
+    , time = Time.millisToPosix 0
+    , string = ""
     }
   , initCmd)
 
 initWinner : Winner
-initWinner = { player = "", indexes = [-1,-1,-1] }
+initWinner = { player = "", indexes = [-1, -1, -1] }
 
 generateNewAutomoves : Int -> List Int
 generateNewAutomoves seed =
@@ -74,14 +87,19 @@ generateNewRandomNumber = (Random.generate NewRandomNumber (Random.int 0 Random.
 
 -- get an initial random number in model.rand and update model.automoves
 initCmd : Cmd Msg
-initCmd = generateNewRandomNumber
+initCmd = Cmd.batch
+    [ Task.perform AdjustTimeZone Time.here -- get current tz
+    , Task.perform Tick Time.now            -- get current localtime
+    , generateNewRandomNumber
+    ]
+
 
 -- SUBS
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+  Time.every 1000 Tick
 
 
 -- UPDATE
@@ -93,23 +111,63 @@ type Msg
   | Auto
   | GenerateRandomNumber
   | NewRandomNumber Int
+  | Trigger
+  | Red
+  | Green
+  | Blue
+  | White
+  | ColorCycling Bool
+  | Tick Time.Posix
+  | AdjustTimeZone Time.Zone
 
+
+
+cycleColors : Model -> Cmd Msg
+cycleColors model =
+    Delay.sequenceIf (not model.colorCycling) <|
+        Delay.withUnit Delay.Millisecond
+            [ ( 0, ColorCycling True )
+            , ( 0, Red )
+            , ( 2000, Green )
+            , ( 2000, Blue )
+            , ( 2000, White )
+            , ( 2000, ColorCycling False )
+            ]
+
+
+{--
+after : Float -> TimeUnit -> msg -> Cmd msg
+Delays an update (with a message) by a given amount of time
+
+after 500 Millisecond DelayedMsg
+
+To trigger a single update after a period of time pass Delay.after as a command to the elm runtime:
+
+FirstMessage ->
+    model ! [ Delay.after 500 Millisecond SecondMessage ]
+--}
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     GenerateRandomNumber ->
       ( model, generateNewRandomNumber )
+
     NewRandomNumber newRandomNumber ->
       ( {model | rand = newRandomNumber, automoves = generateNewAutomoves newRandomNumber}, Cmd.none )
+
     Auto ->
       case model.automoves of
         x :: xs ->
-          let (m2, c2) = update (Cell x) {model | automoves = xs}
-          in (update Auto m2)
+          let
+            (newModel, newCmd) = update (Cell x) {model | automoves = xs}
+          in
+            (newModel, Delay.after 500 Delay.Millisecond Auto)
         _ ->
           (model, Cmd.none)
+
     Restart ->
       init ()
+
     Cell index ->
       if model.winner.player /= "" || cellAt index model.board /= "" then
           (model, Cmd.none)
@@ -118,8 +176,41 @@ update msg model =
           newBoard = Array.set index (getPlayer model.board) model.board
           newWinner = getWinner newBoard
           newModel = { model | board = newBoard, winner = newWinner }
+          newCmd =
+            if newWinner.player == "" then
+              Cmd.none
+            else
+              cycleColors model
         in
           (newModel, Cmd.none)
+
+    Trigger ->
+        ( model, cycleColors model )
+
+    Red ->
+        ( { model | color = "#BC1F31" }, Cmd.none )
+
+    Green ->
+        ( { model | color = "#259860" }, Cmd.none )
+
+    Blue ->
+        ( { model | color = "#1E70B5" }, Cmd.none )
+
+    White ->
+        ( { model | color = "" }, Cmd.none )
+
+    ColorCycling bool ->
+        ( { model | colorCycling = bool }, Cmd.none )
+
+    Tick newTime ->
+      ( { model | time = newTime }
+      , Cmd.none
+      )
+
+    AdjustTimeZone newZone ->
+      ( { model | zone = newZone }
+      , Cmd.none
+      )
 
 
 -- VIEW
@@ -135,14 +226,19 @@ view model =
         "Tie Game"
       else
         "Player: " ++ getPlayer model.board
+    hour   = String.fromInt (Time.toHour   model.zone model.time)
+    minute = String.fromInt (Time.toMinute model.zone model.time)
+    second = String.fromInt (Time.toSecond model.zone model.time)
+    hhmmss = hour ++ ":" ++ minute ++ ":" ++ second
   in
     div [] [
         h1 [] [ text "Tic Tac Toe in Elm" ]
+      , h3 [] [ text hhmmss ]
       , h2 [] [ text status ]
-      -- , div [ animate ] [ text "Hello world!"]
       , button [ onClick Restart ] [ text "Restart"]
       , button [ onClick GenerateRandomNumber ] [ text "New Battle Plan"]
       , button [ onClick Auto ] [ text "Battle"]
+      , button [ onClick Trigger ] [ text "Cycle color" ]
       , div [] [ text "battleplan: ", text <| Debug.toString model.automoves ]
       -- , div [] [ text "winner: ", text <| Debug.toString model.winner ]
       -- , div [] [ text "rand: ", text <| String.fromInt model.rand ]
@@ -150,9 +246,6 @@ view model =
       , div [] [ text "Moves left: ", text <| String.fromInt <| movesLeft model.board ]
       , board model
     ]
-
--- animate = class "w3-container w3-center w3-animate-top"
-animate = class "w3-animate-zoom"
 
 movesUsed : Board -> Int
 movesUsed brd =
@@ -170,7 +263,7 @@ getPlayer brd =
 
 board : Model -> Html Msg
 board model =
-  table styledTable
+  table (styledTable model)
     [ tr [] (List.map (\i -> square i model) [0, 1, 2])
     , tr [] (List.map (\i -> square i model) [3, 4, 5])
     , tr [] (List.map (\i -> square i model) [6, 7, 8])
@@ -183,7 +276,7 @@ square index model =
 cellAt : Int -> Board -> String
 cellAt idx brd = Array.get idx brd |> Maybe.withDefault "?"
 
-styledTable =
+styledTable model =
   [ style "width" "200px"
   , style "height" "200px"
   , style "border" "1px solid black"
@@ -198,6 +291,7 @@ styledTd idx model msg =
   , style "height" "33%"
   , style "background-color" (playerBackgroundColor idx model)
   , style "animation-duration" "2s"
+  , style "color" model.color
   , class (playerClass idx model)
   , onClick msg
   ]
@@ -212,7 +306,6 @@ playerClass idx model =
       "w3-container w3-animate-fading"
     else
       "w3-container w3-animate-opacity"
-
 
 playerBackgroundColor idx model =
   case cellAt idx model.board of
